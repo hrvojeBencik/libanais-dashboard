e775a024be22f2ced027a92329a2e25443d1e5a4
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2024-03-27 23:03:24 +0100

    update ingredients page

--- app/(pages)/ingredients/page.tsx
+++ app/(pages)/ingredients/page.tsx
@@ -1,12 +1,34 @@
-import PageTitle from "@/app/_components/elements/PageTitle/PageTitle";
+"use client";
+import IngredientForm from "@/app/_components/modules/IngredientForm/IngredientForm";
+import PageHeader from "@/app/_components/elements/PageHeader/PageHeader";
+import { useState } from "react";
 
 const Ingredients = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleIsOpen = () => {
+        setIsOpen(true);
+    };
+
+    const handleClose = () => {
+        setIsOpen(false);
+    };
+
     return (
         <div>
-            <PageTitle
-                title="Ingerdient List"
-                subtitle="Efficiently manage ingredients!"
+            <PageHeader
+                title="Ingredient List"
+                subtitle="Efficienlty manage ingredients!"
+                buttonLabel="Add Ingredient"
+                handleIsOpen={handleIsOpen}
             />
+
+            {isOpen && (
+                <IngredientForm
+                    text="Add Ingredient"
+                    handleClose={handleClose}
+                />
+            )}
         </div>
     );
 };
