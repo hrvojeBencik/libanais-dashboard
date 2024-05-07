131af49ba87fe8e26b7bf84bd2f7d5baf9233c45
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2024-05-07 20:04:31 +0200

    Refactor RecipeCard component

--- app/_components/elements/RecipeCard/RecipeCard.tsx
+++ app/_components/elements/RecipeCard/RecipeCard.tsx
@@ -1,28 +1,40 @@
-import Image from "next/image";
-import baklava from "public/assets/baklava.png";
 import DefaultButton from "../DefaultButton/DefaultButton";
-const RecipeCard = () => {
+import { Ingredient } from "@/app/_interfaces/Ingredient";
+
+interface RecipeCardProps {
+    name: string;
+    description: string;
+    ingredients: Ingredient[];
+    image: string;
+}
+const RecipeCard = ({
+    name,
+    description,
+    ingredients,
+    image,
+}: RecipeCardProps) => {
+    const ingredientList = ingredients
+        .map((ingredient) => ingredient.name)
+        .join(", ");
+
     return (
-        <div className="w-full flex bg-white rounded-xl p-4 justify-between">
+        <div className="w-full flex bg-white rounded-xl p-[18px] mb-[36px] justify-between">
             <div>
                 <h4 className=" text-brown-coffee">
-                    Ingredients: Phyllo Dough, Clarified Butter, Pistachios,
-                    Walnuts, Sugar Syrup
+                    Ingredients: {ingredientList}
                 </h4>
                 <h2 className=" text-black-chocolate text-lg font-bold my-1">
-                    47 Pieces Assorted Baklava - Original
+                    {name}
                 </h2>
-                <h4 className=" text-brown-coffee">
-                    Rich and indulgent dessert
-                </h4>
+                <h4 className=" text-brown-coffee">{description}</h4>
                 <DefaultButton
                     text="Edit Recipe"
                     className="rounded-[18px] py-1.5 px-14 mt-[18px]"
                 />
             </div>
             <div className="relative overflow-hidden rounded-xl">
-                <Image
-                    src={baklava}
+                <img
+                    src={image}
                     alt=""
                     className="max-h-44 w-auto"
                 />
