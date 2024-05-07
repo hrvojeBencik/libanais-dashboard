c54a72e60345b455fa0420a11c664f70ca8b84d9
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2024-05-07 21:50:57 +0200

    Add placeholders

--- app/(pages)/employees/page.tsx
+++ app/(pages)/employees/page.tsx
@@ -1,5 +1,12 @@
+import Person from "@/public/assets/svg/person";
+
 const Employees = () => {
-  return <div>Employees</div>;
+    return (
+        <div className="h-[1200px] w-full flex justify-center items-center text-2xl">
+            <Person className=" w-[50px]" />
+            Employees
+        </div>
+    );
 };
 
 export default Employees;
