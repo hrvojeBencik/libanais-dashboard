dea6310252a8b697b925c41a1b7bd93dbe259a4c
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2024-06-03 16:03:57 +0200

    Remove unnecessary code

--- app/_helpers/useOpenForm.tsx
+++ app/_helpers/useOpenForm.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { useState } from "react";
 import { SidebarContext } from "@/app/_components/modules/Sidebar/SidebarHandler";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 
 const useOpenForm = (initialState = false) => {
-    const SCREEN_WIDTH_LIMIT_FOR_MOBILE = 990;
     const [isOpen, setIsOpen] = useState(initialState);
 
     const { setOpenSidebar } = useContext(SidebarContext);
