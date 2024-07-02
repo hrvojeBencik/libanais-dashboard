65308338d2d97df0fe59022e5136ada736a608e1
Dorja Skenderovic <dorjaskenderovic@gmail.com>
2024-07-02 13:05:56 +0200

    Add lot numbers on recipe card

--- app/_components/elements/RecipeCard/RecipeCard.tsx
+++ app/_components/elements/RecipeCard/RecipeCard.tsx
@@ -9,10 +9,6 @@ interface RecipeCardProps {
 const RecipeCard = ({ recipe }: RecipeCardProps) => {
     const { setOpenForm, setEditFormData } = useContext(FormContext);
 
-    const ingredientList = recipe.ingredients
-        .map((ingredient) => ingredient.name)
-        .join(", ");
-
     const handleClick = () => {
         setEditFormData(recipe);
         setOpenForm(true);
@@ -20,11 +16,22 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
     };
 
     return (
-        <div className="flex bg-white rounded-xl p-[18px] sm:p-4 justify-between sm:flex-col-reverse box-border">
-            <div>
-                <h4 className=" text-brown-coffee sm:mt-4 sm:text-sm">
-                    Ingredients: {ingredientList}
-                </h4>
+        <div className="flex  bg-white rounded-xl p-[18px] sm:p-4 justify-between sm:flex-col-reverse box-border">
+            <div className="flex-1">
+                <div className=" text-brown-coffee sm:mt-4 sm:text-sm sm:flex-col sm:items-start flex ">
+                    <h4>Ingredients: &nbsp;</h4>
+                    {recipe.ingredients.map((ingredient, index) => (
+                        <div
+                            key={index}
+                            className="flex flex-col ml-4 sm:mb-1"
+                        >
+                            <span>{ingredient.name}</span>
+                            <span className="text-[10px]">
+                                LOT # {ingredient.lotNumber}
+                            </span>
+                        </div>
+                    ))}
+                </div>
                 <h2 className=" text-black-chocolate text-lg font-bold my-1 sm:my-5 sm:text-sm">
                     {recipe.name}
                 </h2>
@@ -37,7 +44,7 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
                     onClick={handleClick}
                 />
             </div>
-            <div className="relative overflow-hidden rounded-xl">
+            <div className="relative overflow-hidden rounded-xl flex-shrink-0 ml-4">
                 <Image
                     src={recipe.imageUrl}
                     width={347}
@@ -45,7 +52,7 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
                     priority
                     alt=""
                     sizes="(max-width: 920px) 100vw, 15vw"
-                    className="max-h-44 max-w-80 w-auto h-auto sm:w-full sm:max-w-full sm:max-h-24 object-cover object-center"
+                    className="max-h-44 max-w-80  w-auto h-auto sm:w-full sm:max-w-full sm:max-h-24 object-cover object-center"
                 />
                 <div className="absolute inset-0 h-[347px] bg-gradient-to-b from-transparent to-black "></div>
             </div>
